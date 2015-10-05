eccc55ca14bc714125d5c8c73f4a708ff7fa3bcf
Tania Ferman <tania_ferman@hotmail.com>
2015-10-05 19:30:44 +0000

    first commit

---
+++ readlines.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require 'open-uri'
+
+url = "http://ruby.bastardsbook.com/files/fundamentals/hamlet.txt"
+#puts open(url).readline
+local_fname = "hamlet.txt"
+File.open(local_fname, "w"){|file| file.write(open(url).read)}
+
+File.open(local_fname, "r") do |file|
+  file.readlines.each_with_index do |line, idx|
+    puts line if idx % 42 == 41
+  end
+end
